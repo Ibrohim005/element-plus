617333003e48bcfb0c23051d3de7b5060cceaf08
Ibrohim005 <81626279+Ibrohim005@users.noreply.github.com>
2022-07-02 11:46:03 +0500

    Create kr.ts

---
+++ packages/locale/lang/kr.ts
@@ -0,0 +1,121 @@
+export default {
+  name: "kr",
+  el: {
+    colorpicker: {
+      confirm: "Qabul qilish",
+      clear: "Tozalash"
+    },
+    datepicker: {
+      now: "Házir",
+      today: "Búgin",
+      cancel: "Bıykarlaw",
+      clear: "Tazalaw",
+      confirm: "Qabıllaw",
+      selectDate: "Kunni tańlaw",
+      selectTime: "Saattı tańlaw ",
+      startDate: "Baslanıw sánesi",
+      startTime: "Baslanıw waqıtı",
+      endDate: "Tawısıw sánesi",
+      endTime: "Tawısıw waqıtı",
+      prevYear: "Ótken jıl",
+      nextYear: "Kelesi jıl",
+      prevMonth: "Ótken ay",
+      nextMonth: "Kelesi ay",
+      year: "Jıl",
+      month1: "Yanvar",
+      month2: "Fevral",
+      month3: "Mart",
+      month4: "Aprel",
+      month5: "May",
+      month6: "Iyun",
+      month7: "Iyul",
+      month8: "Avgust",
+      month9: "Sentyabr",
+      month10: "Oktyabr",
+      month11: "Noyabr",
+      month12: "Dekabr",
+      week: "Hápte",
+      weeks: {
+        sun: "Yak",
+        mon: "Dush",
+        tue: "Sesh",
+        wed: "Chor",
+        thu: "Pay",
+        fri: "Jum",
+        sat: "Shan"
+      },
+      months: {
+        jan: "Yan",
+        feb: "Fev",
+        mar: "Mar",
+        apr: "Apr",
+        may: "May",
+        jun: "Iyun",
+        jul: "Iyul",
+        aug: "Avg",
+        sep: "Sen",
+        oct: "Okt",
+        nov: "Noy",
+        dec: "Dek"
+      }
+    },
+    select: {
+      loading: "Yuklanmoqda",
+      noMatch: "Mos ma\u02BClumot yo\u02BBq",
+      noData: "Ma\u02BClumot yo\u02BBq",
+      placeholder: "Tanladizngiz"
+    },
+    cascader: {
+      noMatch: "Mos ma\u02BClumot topilmadi",
+      loading: "Yuklanmoqda",
+      placeholder: "Tanlash",
+      noData: "Ma\u02BClumot yo\u02BBq"
+    },
+    pagination: {
+      goto: "O\u02BBtish",
+      pagesize: "/sahifa",
+      total: "Barchasi {total} ta",
+      pageClassifier: ""
+    },
+    messagebox: {
+      title: "Xabar",
+      confirm: "Qabul qilish",
+      cancel: "Bekor qilish",
+      error: "Xatolik"
+    },
+    upload: {
+      deleteTip: "O\u02BBchirish tugmasini bosib o\u02BBchiring",
+      delete: "O\u02BBchirish",
+      preview: "Oldin ko\u02BBrish",
+      continue: "Davom qilish"
+    },
+    table: {
+      emptyText: "Bo\u02BBsh",
+      confirmFilter: "Qabul qilish",
+      resetFilter: "Oldingi holatga qaytarish",
+      clearFilter: "Jami",
+      sumText: "Summasi"
+    },
+    tree: {
+      emptyText: "Ma\u02BClumot yo\u02BBq"
+    },
+    transfer: {
+      noMatch: "Mos ma\u02BClumot topilmadi",
+      noData: "Ma\u02BClumot yo\u02BBq",
+      titles: ["1-jadval", "2-jadval"],
+      filterPlaceholder: "Kalit so\u02BBzni kiriting",
+      noCheckedFormat: "{total} ta element",
+      hasCheckedFormat: "{checked}/{total} ta belgilandi"
+    },
+    image: {
+      error: "Xatolik"
+    },
+    pageHeader: {
+      title: "Orqaga"
+    },
+    popconfirm: {
+      confirmButtonText: "Yes",
+      cancelButtonText: "No"
+    }
+  }
+};
